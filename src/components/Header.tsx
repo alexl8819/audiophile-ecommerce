6fe1ab7eeff9819bde11934deaea411c12c234a3
alexl8819 <124635195+alexl8819@users.noreply.github.com>
2025-01-18 01:11:32 -0800

    wraps link around main site logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
-import type { FC } from "react";
+import { type FC } from "react";
+import { Link } from 'react-aria-components';
 import { StyledIconButton } from "./Button";
 
 import logo from '../assets/shared/desktop/logo.svg';
@@ -6,10 +7,13 @@ import logo from '../assets/shared/desktop/logo.svg';
 interface HeaderProps {}
 
 export const Header: FC<HeaderProps> = ({}) => {
+    // const [isOpen, setIsOpen] = useState<boolean>(false);
     return (
         <header className="flex flex-row justify-evenly bg-black py-6">
             <StyledIconButton iconName="icon-hamburger" viewportModifier="tablet" altText="navbar icon" onPress={() => {}} />
-            <img src={logo.src} alt="Audiophile Main Site Logo" loading="eager" />
+            <Link href="/">
+                <img src={logo.src} alt="Audiophile Logo" loading="eager" />
+            </Link>
             <StyledIconButton iconName="icon-cart" altText="shopping cart" onPress={() => {}} />
         </header>
     )
