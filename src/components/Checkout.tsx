e9a8de1380a1383d08fd3512e06b80d6b3a69bb0
alexl8819 <124635195+alexl8819@users.noreply.github.com>
2025-01-27 20:07:16 -0800

    implements vat calculation for checkout

--- src/components/Checkout.tsx
+++ src/components/Checkout.tsx
@@ -1,9 +1,10 @@
-import { type FC } from 'react';
+import { type FC, useState, useEffect } from 'react';
 import { Elements } from '@stripe/react-stripe-js';
+import countryVat from 'country-vat';
+
 import { OrderForm } from './forms/OrderForm';
 import stripeClient from '../lib/stripe'; 
 import { CartSummary } from './Cart';
-
 import { cartItems } from '../stores/cart';
 import { useStore } from '@nanostores/react';
 
@@ -13,16 +14,37 @@ interface CheckoutProps {
 }
 
 export const CheckoutPreview: FC<CheckoutProps> = ({ amountDue, shippingCost }) => {
+    const [total, setTotal] = useState<number>(-1);
+    const [vatRate, setVatRate] = useState<number>(0);
+    
     const items = useStore(cartItems);
+
+    const handleCountrySet = (countryCode: string) => {
+        const rate = countryVat(countryCode);
+        
+        if (!rate) {
+            return;
+        }
+
+        setVatRate(rate);
+    };
+
+    useEffect(() => {
+        setTotal(amountDue + (vatRate > 0 ? (amountDue * vatRate) : 0) + shippingCost);
+    }, [vatRate]);
+
+    if (total < 0) {
+        return null;
+    }
 
     return (
         <Elements stripe={stripeClient} options={{
             mode: 'payment',
-            amount: amountDue + shippingCost,
+            amount: total,
             currency: 'usd'
         }}>
-            <OrderForm>
-                <CartSummary items={items} shippingFee={shippingCost} />
+            <OrderForm onCountrySet={handleCountrySet}>
+                <CartSummary items={items} shippingFee={shippingCost} vatRate={vatRate} />
             </OrderForm>
         </Elements>
     )
