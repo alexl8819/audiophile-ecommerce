9618454f1d23432989c45750da8f1e25783891d4
alexl8819 <124635195+alexl8819@users.noreply.github.com>
2025-01-28 17:24:59 -0800

    fix: supports header resizing up to tablet by redesigning layout structure

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,11 +15,15 @@ export const Header: FC<HeaderProps> = ({}) => {
     const items = useStore(cartItems);
     
     return (
-        <header className="flex flex-row justify-evenly bg-black py-6">
-            <StyledIconButton iconName="icon-hamburger" viewportModifier="tablet" altText="navbar icon" onPress={() => {}} />
-            <Link href="/"><img src={logo.src} alt="Audiophile Logo" loading="eager" /></Link>
-            <StyledIconButton iconName="icon-cart" altText="shopping cart" onPress={() => toggleCart()} />
-            <Cart items={items} />
+        <header className="flex flex-row justify-evenly md:justify-between bg-black md:px-8 py-6">
+            <div className="flex flex-row justify-between w-52">
+                <StyledIconButton iconName="icon-hamburger" viewportModifier="tablet" altText="navbar icon" onPress={() => {}} />
+                <Link href="/"><img src={logo.src} alt="Audiophile Logo" loading="eager" /></Link>
+            </div>
+            <div className="w-auto">
+                <StyledIconButton iconName="icon-cart" altText="shopping cart" onPress={() => toggleCart()} />
+                <Cart items={items} />
+            </div>
         </header>
     )
 }
