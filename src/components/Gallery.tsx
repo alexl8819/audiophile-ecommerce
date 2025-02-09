24e6bcdbccd2d47f7bd5e3996ca3199cdc6cda6b
alexl8819 <124635195+alexl8819@users.noreply.github.com>
2025-02-08 23:55:53 -0800

    fix: removes bottom margin on all ProductDisplay components, enlarges images to scale to full height and width

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -53,14 +53,14 @@ export const ProductShowcase: FC<ProductShowcaseProps> = memo(({ name, path, tar
             {
                 mobileThumbnail && tabletThumbnail && desktopThumbnail ? 
                 (
-                    <figure className='bg-light-gray flex flex-col justify-center items-center mb-6'>
+                    <figure className='bg-light-gray flex flex-col justify-center items-center'>
                         <picture>
                             <source srcSet={mobileThumbnail} media="(max-width: 767px)" className={`${styles && styles.roundedEdges ? 'rounded-lg' : ''}`} />
                             <source srcSet={tabletThumbnail} media="(max-width: 1023px)" className={`${styles && styles.roundedEdges ? 'rounded-lg' : ''}`} />
                             <source srcSet={desktopThumbnail} media="(min-width: 1024px)" className={`${styles && styles.roundedEdges ? 'rounded-lg' : ''}`} />
                             <img 
                                 className={
-                                    `w-auto h-auto ${styles && styles.roundedEdges ? 'rounded-lg' : ''}`
+                                    `w-auto md:w-full h-auto md:h-full ${styles && styles.roundedEdges ? 'rounded-lg' : ''}`
                                 } 
                                 src={mobileThumbnail} 
                                 alt={`${name} product preview`} 
