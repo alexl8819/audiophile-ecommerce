00aab6a6e4a2ae22a90b669b7a373d87f7b5ac24
alexl8819 <124635195+alexl8819@users.noreply.github.com>
2025-07-24 19:24:49 -0700

    adds aws secrets manager, adds secrets implementation, modifies and removes old secrets added from build env

---
+++ src/lib/secrets.ts
@@ -0,0 +1,37 @@
+import {
+  SecretsManagerClient,
+  GetSecretValueCommand,
+} from '@aws-sdk/client-secrets-manager';
+
+const DEV = process.env.NODE_ENV !== 'production';
+
+async function decryptSecrets (secretId: string, region = 'us-east-1') {
+    const client = new SecretsManagerClient({ region });
+
+    console.log('called');
+
+    let response;
+
+    try {
+        response = await client.send(
+            new GetSecretValueCommand({
+                SecretId: secretId,
+                VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
+            })
+        );
+    } catch (error) {
+        // For a list of exceptions thrown, see
+        // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        throw error;
+    }
+
+    const secretStr = response.SecretString;
+
+    if (secretStr && secretStr.length) {
+        return JSON.parse(secretStr);
+    }
+
+    return {};
+}
+
+export const secrets = await decryptSecrets(!DEV ? process.env.VAULT_NAME : import.meta.env.VAULT_NAME);
